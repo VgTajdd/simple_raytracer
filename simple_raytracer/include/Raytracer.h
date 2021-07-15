85b728adbaabb576e46dc02a58f8a9f9c4f67851
VgTajdd <aduranddiaz@gmail.com>
2021-07-14 21:43:17 -0500

    Add .clang-format and format files

--- simple_raytracer/include/Raytracer.h
+++ simple_raytracer/include/Raytracer.h
@@ -8,6 +8,7 @@
 #define RAYTRACER_H_
 
 #include <vector>
+
 #include "Vec3.h"
 
 class Scene;
@@ -15,70 +16,54 @@ class Ray;
 class SceneObject;
 struct Material;
 
-struct IntersectionInfo
-{
-	Vec3 normal;
-	Vec3 pi;
-	SceneObject* object = nullptr;
+struct IntersectionInfo {
+  Vec3 normal;
+  Vec3 pi;
+  SceneObject* object = nullptr;
 };
 
-struct RaytracerImage
-{
-	RaytracerImage( const unsigned int w, const unsigned int h ):
-		width( w ), height( h )
-	{
-		data.resize( h, std::vector<Vec3>() );
-		for ( unsigned int i = 0; i < w; i++ )
-		{
-			data[i].resize( w );
-		}
-	}
-	unsigned int width;
-	unsigned int height;
-	std::vector<std::vector<Vec3>> data;
+struct RaytracerImage {
+  RaytracerImage(const unsigned int w, const unsigned int h)
+      : width(w), height(h) {
+    data.resize(h, std::vector<Vec3>());
+    for (unsigned int i = 0; i < w; i++) {
+      data[i].resize(w);
+    }
+  }
+  unsigned int width;
+  unsigned int height;
+  std::vector<std::vector<Vec3>> data;
 };
 
-class RaytracerImageSaver
-{
-public:
-	enum class FORMAT
-	{
-		PNG,
-		PPM
-	};
-	static bool save( const RaytracerImage& image, const char* filename, const FORMAT& format = FORMAT::PNG );
+class RaytracerImageSaver {
+ public:
+  enum class FORMAT { PNG, PPM };
+  static bool save(const RaytracerImage& image, const char* filename,
+                   const FORMAT& format = FORMAT::PNG);
 };
 
-class Raytracer
-{
-public:
-	explicit Raytracer( const unsigned int w, unsigned int h ) : m_width( w ), m_height( h ) {}
-	void setEyeLocation( const Vec3& eyeLocation )
-	{
-		m_eyeLocation = eyeLocation;
-	}
+class Raytracer {
+ public:
+  explicit Raytracer(const unsigned int w, unsigned int h)
+      : m_width(w), m_height(h) {}
+  void setEyeLocation(const Vec3& eyeLocation) { m_eyeLocation = eyeLocation; }
 
-	RaytracerImage raytrace( const Scene& scene, const int maxDepth );
+  RaytracerImage raytrace(const Scene& scene, const int maxDepth);
 
-private:
-	Vec3 computeLight( const Vec3& direction,
-					   const Vec3& lightcolor,
-					   const Vec3& normal,
-					   const Vec3& halfvec,
-					   const Material& material );
+ private:
+  Vec3 computeLight(const Vec3& direction, const Vec3& lightcolor,
+                    const Vec3& normal, const Vec3& halfvec,
+                    const Material& material);
 
-	IntersectionInfo intersect( const Scene& scene,
-								const Ray& ray );
+  IntersectionInfo intersect(const Scene& scene, const Ray& ray);
 
-	Vec3 computeColor( const Scene& scene,
-					   const Ray& ray,
-					   const IntersectionInfo& intersection,
-					   const int maxDepth );
+  Vec3 computeColor(const Scene& scene, const Ray& ray,
+                    const IntersectionInfo& intersection, const int maxDepth);
 
-private:
-	Vec3 m_eyeLocation;
-	unsigned int m_width;
-	unsigned int m_height;
+ private:
+  Vec3 m_eyeLocation;
+  unsigned int m_width;
+  unsigned int m_height;
 };
 
-#endif // !RAYTRACER_H_
+#endif  // !RAYTRACER_H_
--- simple_raytracer/src/main.cpp
+++ simple_raytracer/src/main.cpp
@@ -7,33 +7,33 @@
 #include "Raytracer.h"
 #include "Scene.h"
 
-int main( int argc, char* argv[] )
-{
-	const int WIDTH = 500;
-	const int HEIGHT = 500;
+int main(int argc, char* argv[]) {
+  const int WIDTH = 500;
+  const int HEIGHT = 500;
 
-	Scene scene;
+  Scene scene;
 
-	auto sphere1 = ObjectsFactory::createSphere( Vec3( 100, 0, -250 ), 100 );
-	sphere1->pvtMaterial().diffuse = Vec3( 0.8f, 0.2f, 0.2f );
-	scene.addObject( sphere1 );
+  auto sphere1 = ObjectsFactory::createSphere(Vec3(100, 0, -250), 100);
+  sphere1->pvtMaterial().diffuse = Vec3(0.8f, 0.2f, 0.2f);
+  scene.addObject(sphere1);
 
-	auto sphere2 = ObjectsFactory::createSphere( Vec3( -100, 0, -100 ), 100 );
-	sphere2->pvtMaterial().diffuse = Vec3( 0.2f, 0.8f, 0.2f );
-	scene.addObject( sphere2 );
+  auto sphere2 = ObjectsFactory::createSphere(Vec3(-100, 0, -100), 100);
+  sphere2->pvtMaterial().diffuse = Vec3(0.2f, 0.8f, 0.2f);
+  scene.addObject(sphere2);
 
-	auto triangle = ObjectsFactory::createTriangle( Vec3( 300, -100, 400 ), Vec3( 0, -100, -600 ), Vec3( -300, -100, 400 ) );
-	scene.addObject( triangle );
+  auto triangle = ObjectsFactory::createTriangle(
+      Vec3(300, -100, 400), Vec3(0, -100, -600), Vec3(-300, -100, 400));
+  scene.addObject(triangle);
 
-	scene.addLight( Vec3( -200, 0, 200 ), Vec3( 1, 1, 1 ), true );
+  scene.addLight(Vec3(-200, 0, 200), Vec3(1, 1, 1), true);
 
-	Raytracer raytracer( WIDTH, HEIGHT );
-	raytracer.setEyeLocation( Vec3( 0, 0, 1000 ) );
-	RaytracerImage image = raytracer.raytrace( scene, 2 );
+  Raytracer raytracer(WIDTH, HEIGHT);
+  raytracer.setEyeLocation(Vec3(0, 0, 1000));
+  RaytracerImage image = raytracer.raytrace(scene, 2);
 
-	bool ok = RaytracerImageSaver::save( image, "output.ppm", RaytracerImageSaver::FORMAT::PPM );
-	if ( ok )
-		system( "output.ppm" );
+  bool ok = RaytracerImageSaver::save(image, "output.ppm",
+                                      RaytracerImageSaver::FORMAT::PPM);
+  if (ok) system("output.ppm");
 
-	return 0;
+  return 0;
 }
